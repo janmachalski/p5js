5317a2a340f5fd09c71d1090a21b38daa3ad5297
dxxbspace <61709086+dxxbspace@users.noreply.github.com>
2020-09-15 18:32:58 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,8 @@
 // JUST INTONATION HARMONY ENGINE
 // copyright Jan Machalski 2019
-// version 2020_08_15
+// version 2020_09_15
 // made in P5JS
+// prime factorization version
 
 const iloscGlosow = 3;
 const modusInit = 3; // three logarithmic/arithmetic (otonal/utonal) intervals per octave
@@ -9,13 +10,18 @@ const freqInit = 220.0; // reference frequency from which the rest is being buil
 let utonalInit = false; // false=major true=minor first chord switch (negative harmony switch)
 const tuneHue = 530.0; // tune audio and colour spectrum if needed
 
-const maxModus = 5;
-const maxEpimeric = 10;
+const primeLimit = 7;
+const maxModus = 8;
+let ukladRozlegly = false;
+const kolorTekstu = 255;
+let instrukcja = true;
+let saturacja = 100;
+let swiatlo = 55;
 let dzwiek = 0; //audio off przy starcie
 let glosy = []; //wysokosci dzwiekow i obliczenia ich
 let syntezatory = []; //oscylatory
 let liczbaPierwsza = []; //array liczb pierwszych
-let iToEpimeric = []; //dictionary of factorizations. Product (p/(p-1))^n
+let iToPrime = []; //dictionary of prime factorizations
 let reverb;
 let reverbTime = 5;
 
@@ -27,7 +33,7 @@ function setup() {
       j += 1;
     }
   }
-  iToEpimericInit();
+  initPrimeFactorization();
   for (let i = 0; i < iloscGlosow; i++) { //tworzy glosy po arpeggiu
     glosy[i] = new Glos();
     for (let j = 0; j < i; j++) {
@@ -47,22 +53,42 @@ function setup() {
 function draw() {
   background(220);
   noStroke();
+
   //rysuj prostokaty
   for (let i = 0; i < iloscGlosow; i++) {
-    fill(freqToHue(glosy[i].freq), 150, 155);
+    fill(freqToHue(glosy[i].freq), saturacja, swiatlo);
     rect(0, (height * (iloscGlosow - 1 - i) / iloscGlosow), width, height / iloscGlosow);
   }
-  //rysuj rownania
-  for (let i = 0; i < iloscGlosow; i++) {
-    fill(0, 0, 0);
-    text(glosy[i].string, 25, (height * (iloscGlosow - i) / iloscGlosow) - 10);
+
+  fill(kolorTekstu);
+
+  switch (instrukcja) {
+    case true:
+      let wierszeOdstep = 20;
+      textSize(15);
+      text("Click left mouse button to start (left click or press n to restart).", 20, wierszeOdstep);
+      text("Press left and right arrows to arpeggiate chords", 20, wierszeOdstep * 2);
+      text("Press 1 to change between utonality and otonality.", 20, wierszeOdstep * 3);
+      text("Press Q A W S E  or D for basic modulations. Alternate between keys and arrows", 20, wierszeOdstep * 4);
+      text("Press B to switch between closed and open voicings", 20, wierszeOdstep * 5);
+      text("Press up and down arrows to change the modus. Voices might overlap.", 20, wierszeOdstep * 6);
+      text("R F T G Y H are the shortcuts for pressing 1 and qawsed simultaneously.", 20, wierszeOdstep * 7);
+      text("6 Y H N U J are like left and right arrow but work for 1 voice only.", 20, wierszeOdstep * 8);
+      // wyswietl numery glosow
+      for (let i = 0; i < iloscGlosow; i++) {
+        text(i, 5, height * (iloscGlosow - i) / iloscGlosow);
+      }
+      break;
+
+    case false:
+      //rysuj rownania
+      textSize(20);
+      for (let i = 0; i < iloscGlosow; i++) {
+        fill(kolorTekstu);
+        text(glosy[i].string, 25, (height * (iloscGlosow - i) / iloscGlosow) - 10);
+      }
+      break;
   }
-  // wyswietl numery glosow
-  for (let i = 0; i < iloscGlosow; i++) {
-    fill(0, 0, 0);
-    text(i, 5, height * (iloscGlosow - i) / iloscGlosow);
-  }
-  text("Press mouse or n to start. Press 1 q a w s e d r f t g y h u j i k and arrows to operate.", 20, 20);
 
   if (dzwiek == 1) {
     for (let i = 0; i < iloscGlosow; i++) {
@@ -84,9 +110,9 @@ class Glos {
     this.skladnik = 0;
     this.utonal = utonalInit;
     this.transposition = freqInit;
-    this.epimeric = [];
-    for (let i = 0; i < maxEpimeric; i++) {
-      this.epimeric[i] = 0; // zero as default factorization
+    this.factorization = []; //prime factors (Monzos)
+    for (let i = 0; i < primeLimit; i++) {
+      this.factorization[i] = 0; //zero as default factorization
     }
     this.string = "new voice loading";
   }
@@ -96,19 +122,17 @@ class Glos {
     let x = 0;
     x += log(this.modus + int(!this.utonal) * this.skladnik);
     x -= log(this.modus + int(this.utonal) * this.skladnik);
-    for (let i = 0; i < maxEpimeric; i++) {
-      x += this.epimeric[i] * log(liczbaPierwsza[i]);
-      x -= this.epimeric[i] * log(liczbaPierwsza[i] - 1);
+    for (let i = 0; i < primeLimit; i++) {
+      x += this.factorization[i] * log(liczbaPierwsza[i]);
     }
     this.freq = this.transposition * exp(x);
     this.matString();
   }
 
-  //prime epimeric math
-  //Np. kwarta to pep(1, -1) bo +1oktawa -1kwinta
-  pep() {
+  //transpose by prime factors
+  monzo() {
     for (let i = 0; i < arguments.length; i++) {
-      this.epimeric[i] += arguments[i];
+      this.factorization[i] += arguments[i];
     }
     this.przelicz();
   }
@@ -118,10 +142,11 @@ class Glos {
     this.modus = modus;
     this.stopien = skladnik;
     for (let i = 0; i < arguments.length - 3; i++) {
-      this.epimeric[i] += arguments[i + 3];
+      this.factorization[i] += arguments[i + 3];
     }
     this.przelicz();
   }
+
   //returns object variables
   info() {
     let stringZmiennych = "transp " + this.transposition
@@ -129,8 +154,8 @@ class Glos {
     stringZmiennych += " modus " + str(this.modus);
     stringZmiennych += " stopien " + str(this.skladnik);
     stringZmiennych += " epimericFactors "
-    for (let i = 0; i < maxEpimeric; i++) {
-      stringZmiennych += " " + str(this.epimeric[i]);
+    for (let i = 0; i < primeLimit; i++) {
+      stringZmiennych += " " + str(this.factorization[i]);
     }
     return stringZmiennych;
   }
@@ -141,7 +166,7 @@ class Glos {
     this.string += " * [(" + this.modus + " + " + int(!this.utonal) * this.skladnik
     this.string += ") / (" + this.modus + " + " + int(this.utonal) * this.skladnik + ")]";
     for (let i = 0; i < 4; i++) {
-      this.string += " * (" + liczbaPierwsza[i] + "/" + (liczbaPierwsza[i] - 1) + ")^" + this.epimeric[i];
+      this.string += " * " + liczbaPierwsza[i] + "^" + this.factorization[i];
     }
     this.string += " = " + nf(this.freq, 0, 3) + "Hz";
   }
@@ -151,10 +176,10 @@ class Glos {
       this.skladnik = this.modus - this.skladnik;
       switch (this.utonal) {
         case false:
-          this.epimeric[0] += 1;
+          this.factorization[0] += 1;
           break;
         case true:
-          this.epimeric[0] -= 1;
+          this.factorization[0] -= 1;
           break;
       }
     }
@@ -168,10 +193,10 @@ class Glos {
       this.skladnik = 0;
       switch (this.utonal) {
         case false:
-          this.epimeric[0] += 1;
+          this.factorization[0] += 1;
           break;
         case true:
-          this.epimeric[0] -= 1;
+          this.factorization[0] -= 1;
           break;
       }
     } else {
@@ -180,16 +205,15 @@ class Glos {
     this.przelicz();
   }
 
-  //przewroty akordow, moze da sie scalic z tym powyzej w jedna funkcje
   count210() { // counting 2, 1, 0 through octaves: if major then down, if minor then up.
     if (this.skladnik <= 0) {
       this.skladnik = this.modus - 1;
       switch (this.utonal) {
         case false:
-          this.epimeric[0] -= 1;
+          this.factorization[0] -= 1;
           break;
         case true:
-          this.epimeric[0] += 1;
+          this.factorization[0] += 1;
           break;
       }
     } else {
@@ -204,7 +228,7 @@ class Glos {
       case false:
         if (this.skladnik == this.modus - 1) {
           this.skladnik = 0;
-          this.epimeric[0] += 1;
+          this.factorization[0] += 1;
         } else {
           this.skladnik += 1;
         }
@@ -212,7 +236,7 @@ class Glos {
       case true:
         if (this.skladnik == 0) {
           this.skladnik = this.modus - 1;
-          this.epimeric[0] += 1;
+          this.factorization[0] += 1;
         } else {
           this.skladnik -= 1;
         }
@@ -226,7 +250,7 @@ class Glos {
       case false:
         if (this.skladnik == 0) {
           this.skladnik = this.modus - 1;
-          this.epimeric[0] -= 1;
+          this.factorization[0] -= 1;
         } else {
           this.skladnik -= 1;
         }
@@ -234,7 +258,7 @@ class Glos {
       case true:
         if (this.skladnik == this.modus - 1) {
           this.skladnik = 0;
-          this.epimeric[0] -= 1;
+          this.factorization[0] -= 1;
         } else {
           this.skladnik += 1;
         }
@@ -244,11 +268,11 @@ class Glos {
   }
 
   transponuj(wpiszLicznik, wpiszMianownik) { //np. z akordu C do D to transponuj(9,8)
-    for (let i = 0; i < iToEpimeric[wpiszLicznik].length; i++) {
-      this.epimeric[i] += iToEpimeric[wpiszLicznik][i];
+    for (let i = 0; i < iToPrime[wpiszLicznik].length; i++) {
+      this.factorization[i] += iToPrime[wpiszLicznik][i];
     }
-    for (let i = 0; i < iToEpimeric[wpiszMianownik].length; i++) {
-      this.epimeric[i] -= iToEpimeric[wpiszMianownik][i];
+    for (let i = 0; i < iToPrime[wpiszMianownik].length; i++) {
+      this.factorization[i] -= iToPrime[wpiszMianownik][i];
     }
     this.przelicz();
   }
@@ -307,12 +331,23 @@ class Glos {
       return i;
     }
   }
+
+  //zostaw same faktoryzacje bez zmiany wysokosci 
+  faktor() { //stopien przechodzi na 0 bez zmiany wysokosci
+    let intA = this.modus + int(!this.utonal) * this.skladnik;
+    let intB = this.modus + int(this.utonal) * this.skladnik;
+    transponuj(intA, intB);
+    this.skladnik = 0;
+    this.przelicz();
+
+  }
+
   reset() {
     this.modus = modusInit;
     this.skladnik = 0;
     this.transposition = freqInit;
-    for (let i = 0; i < 10; i++) {
-      this.epimeric[i] = 0;
+    for (let i = 0; i < primeLimit; i++) {
+      this.factorization[i] = 0;
     }
     this.przelicz();
   }
@@ -336,6 +371,9 @@ function isPrime(int) {
 // USER INTERFACE
 function mousePressed() {
   reset();
+  instrukcja = false;
+  saturacja = 180;
+  swiatlo = 125;
   klikniecieX = mouseX;
   klikniecieY = mouseY;
   if (dzwiek == 0) {
@@ -475,6 +513,25 @@ function keyPressed() {
   } else if (key == "k") {
     glosy[1].nizej();
 
+  } else if (key == "b") { // uklad skupiony i rozlegly
+    switch (ukladRozlegly) {
+      case false:
+        for (let i = 0; i < iloscGlosow; i++) {
+          for (j = 0; j < i; j++) {
+            glosy[i].wyzej();
+          }
+        }
+        break;
+      case true:
+        for (let i = 0; i < iloscGlosow; i++) {
+          for (j = 0; j < i; j++) {
+            glosy[i].nizej();
+          }
+        }
+        break;
+    }
+    ukladRozlegly = !ukladRozlegly;
+
   } else if (keyCode === LEFT_ARROW) {
     for (let i = 0; i < iloscGlosow; i++) {
       glosy[i].nizej();
@@ -505,7 +562,7 @@ function reset() {
     glosy[i].skladnik = 0;
     glosy[i].transposition = freqInit;
     for (let j = 0; j < 10; j++) {
-      glosy[i].epimeric[j] = 0;
+      glosy[i].factorization[j] = 0;
     }
     for (let j = 0; j < i; j++) {
       glosy[i].wyzej();
@@ -513,37 +570,62 @@ function reset() {
     glosy[i].przelicz();
   }
 }
-//LOADS A DICTIONARY OF EPIMORIC FACORIZATION OF INT NUMBERS
-function iToEpimericInit() {
-  // int to prime epimeric factorization: int = [n1, n2, n3, n4...]
-  // int = (2)^n1*(3/2)^n2*(5/4)^n3*(7/6)^n4*(11/10)^n5 etc.
-  iToEpimeric[1] = [0];
-  iToEpimeric[2] = [1];
-  iToEpimeric[3] = [1, 1];
-  iToEpimeric[4] = [2];
-  iToEpimeric[5] = [2, 0, 1];
-  iToEpimeric[6] = [2, 1];
-  iToEpimeric[7] = [2, 1, 0, 1];
-  iToEpimeric[8] = [3];
-  iToEpimeric[9] = [2, 2];
-  iToEpimeric[10] = [3, 0, 1];
-  iToEpimeric[11] = [1, 0, 1, 0, 1];
-  iToEpimeric[12] = [3, 1];
-  iToEpimeric[13] = [3, 1, 0, 0, 0, 1];
-  iToEpimeric[14] = [3, 1, 0, 1];
-  iToEpimeric[15] = [3, 1, 1];
-  iToEpimeric[16] = [4];
-  iToEpimeric[17] = [4, 0, 0, 0, 0, 0, 1];
-  iToEpimeric[18] = [3, 2];
-  iToEpimeric[19] = [3, 2, 0, 0, 0, 0, 0, 1];
-  iToEpimeric[20] = [4, 0, 1];
-  iToEpimeric[21] = [3, 2, 0, 1];
-  iToEpimeric[22] = [4, 0, 1, 0, 1];
-  iToEpimeric[23] = [4, 0, 1, 0, 1, 0, 0, 0, 1];
-  iToEpimeric[24] = [4, 1];
-  iToEpimeric[25] = [4, 2];
-  iToEpimeric[26] = [4, 1, 0, 0, 0, 1];
-  iToEpimeric[27] = [3, 3];
-  iToEpimeric[28] = [4, 1, 0, 1];
-  iToEpimeric[29] = [4, 1, 0, 1, 0, 0, 0, 0, 0, 1];
+
+//LOADS A DICTIONARY OF EPIMORIC INTEGER FACTORIZATIONS (Monzos)
+function initPrimeFactorization() {
+  iToPrime[1] = [0];
+  iToPrime[2] = [1];
+  iToPrime[3] = [0, 1];
+  iToPrime[4] = [2];
+  iToPrime[5] = [0, 0, 1];
+  iToPrime[6] = [1, 1];
+  iToPrime[7] = [0, 0, 0, 1];
+  iToPrime[8] = [3];
+  iToPrime[9] = [0, 2];
+  iToPrime[10] = [1, 0, 1];
+  iToPrime[11] = [0, 0, 0, 0, 1];
+  iToPrime[12] = [2, 1];
+  iToPrime[13] = [0, 0, 0, 0, 0, 1];
+  iToPrime[14] = [1, 0, 0, 1];
+  iToPrime[15] = [0, 1, 1];
+  iToPrime[16] = [4];
+  iToPrime[17] = [0, 0, 0, 0, 0, 0, 1];
+  iToPrime[18] = [1, 2];
+  iToPrime[19] = [0, 0, 0, 0, 0, 0, 0, 1];
+  iToPrime[20] = [2, 0, 1];
+  iToPrime[21] = [0, 1, 0, 1];
+  iToPrime[22] = [1, 0, 0, 0, 1];
+  iToPrime[23] = [0, 0, 0, 0, 0, 0, 0, 0, 1];
+  iToPrime[24] = [3, 1];
+  iToPrime[25] = [0, 0, 2];
+  iToPrime[26] = [1, 0, 0, 0, 0, 1];
+  iToPrime[27] = [0, 3];
+  iToPrime[28] = [2, 0, 0, 1];
+  iToPrime[29] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 1];
+  iToPrime[30] = [1, 1, 1];
+  iToPrime[31] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1];
+  iToPrime[32] = [5];
+  iToPrime[33] = [2, 0, 0, 0, 1];
+  iToPrime[34] = [1, 0, 0, 0, 0, 0, 1];
+  iToPrime[35] = [0, 0, 1, 1];
+  iToPrime[36] = [2, 2];
+  iToPrime[37] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1];
+  iToPrime[38] = [1, 0, 0, 0, 0, 0, 0, 1];
+  iToPrime[39] = [0, 1, 0, 0, 0, 1];
+  iToPrime[40] = [3, 0, 1];
+  iToPrime[41] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1];
+  iToPrime[42] = [1, 1, 0, 1];
+  iToPrime[43] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1];
+  iToPrime[44] = [1, 1, 0, 0, 1];
+  iToPrime[45] = [0, 2, 1];
+  iToPrime[46] = [1, 0, 0, 0, 0, 0, 0, 0, 1];
+  iToPrime[47] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1];
+  iToPrime[48] = [4, 1];
+  iToPrime[49] = [0, 0, 0, 2];
+  iToPrime[50] = [1, 0, 2];
+  iToPrime[51] = [0, 1, 0, 0, 0, 0, 1];
+  iToPrime[52] = [2, 0, 0, 0, 0, 1];
+  iToPrime[53] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1];
+  iToPrime[80] = [4, 0, 1];
+  iToPrime[81] = [0, 4];
 }
